f0816f41068e544aaaf77dbc176f0e7c010426b3
Maciej Szostak <maciek@recostudio.pl>
2022-08-30 17:39:49 +0200

    stackView added with only 1field userName

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LoginView: UIView {
     
+    let stackView = UIStackView()
     let userNameTextField = UITextField()
     
     override init(frame: CGRect) {
@@ -34,19 +35,25 @@ extension LoginView {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
         
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
         userNameTextField.translatesAutoresizingMaskIntoConstraints = false
         userNameTextField.placeholder = "Username"
         userNameTextField.delegate = self
     }
     
     func layout() {
+        stackView.addArrangedSubview(userNameTextField)
         
-        addSubview(userNameTextField )
-        
+        addSubview(stackView )
+ 
         NSLayoutConstraint.activate([
-            userNameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
-            userNameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
-            trailingAnchor.constraint(equalToSystemSpacingAfter:  userNameTextField.trailingAnchor, multiplier: 1 )
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter:  stackView.trailingAnchor, multiplier: 1 ),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
         ])
     }
 }
