6db33c9635d234d90f9db8509ee98cc8e2ffac1f
Maciej Szostak <maciek@recostudio.pl>
2022-08-31 13:26:01 +0200

    separator added plus rounded corners

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -13,6 +13,7 @@ class LoginView: UIView {
     let stackView = UIStackView()
     let userNameTextField = UITextField()
     let passwordTextField = UITextField()
+    let dividerView  = UIView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,7 +31,7 @@ extension LoginView {
     
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .orange
+        backgroundColor = .secondarySystemBackground
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
@@ -44,10 +45,17 @@ extension LoginView {
         passwordTextField.placeholder = "Password"
         passwordTextField.isSecureTextEntry = true
         passwordTextField.delegate = self
+        
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .secondarySystemFill
+        
+        layer.cornerRadius = 5
+        clipsToBounds = true 
     }
     
     func layout() {
         stackView.addArrangedSubview(userNameTextField)
+        stackView.addArrangedSubview(dividerView)
         stackView.addArrangedSubview(passwordTextField)
         
         addSubview(stackView )
@@ -58,6 +66,8 @@ extension LoginView {
             trailingAnchor.constraint(equalToSystemSpacingAfter:  stackView.trailingAnchor, multiplier: 1 ),
             bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
         ])
+        // another way to add constraint: this one for divide bar, because we need only high constraint
+        dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
 }
 // MARK: UITextFieldDelegate
