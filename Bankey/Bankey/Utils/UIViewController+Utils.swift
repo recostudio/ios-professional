dc2355a54f0a89b7723d4ebe7fd5f1f4891104fe
Maciek <46852030+recostudio@users.noreply.github.com>
2022-11-24 16:47:04 +0100

    fix

--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -25,4 +25,10 @@ extension UIViewController {
         UINavigationBar.appearance().standardAppearance = navBarAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
+    
+        func setTabBarImage(imageName: String, title: String) {
+            let configuration = UIImage.SymbolConfiguration(scale: .large)
+            let image = UIImage(systemName: imageName, withConfiguration: configuration)
+            tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+        }
 }
