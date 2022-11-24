faa0354f4277f863b88dd8ca90f6331f00597f51
Maciek <46852030+recostudio@users.noreply.github.com>
2022-11-24 16:12:31 +0100

    fix: deprecated method

--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -1,18 +1,28 @@
 import UIKit
 
+//extension UIViewController {
+//    func setStatusBar() {
+//        let statusBarSize = UIApplication.shared.statusBarFrame.size
+//        let frame = CGRect(origin: .zero, size: statusBarSize)
+//        let statusbarView = UIView(frame: frame)
+//
+//        statusbarView.backgroundColor = appColor
+//        view.addSubview(statusbarView)
+//    }
+//
+//    func setTabBarImage(imageName: String, title: String) {
+//        let configuration = UIImage.SymbolConfiguration(scale: .large)
+//        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+//        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+//    }
+//}
+// to fix deprecated method: 
 extension UIViewController {
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusbarView = UIView(frame: frame)
-        
-        statusbarView.backgroundColor = appColor
-        view.addSubview(statusbarView)
-    }
-    
-    func setTabBarImage(imageName: String, title: String) {
-        let configuration = UIImage.SymbolConfiguration(scale: .large)
-        let image = UIImage(systemName: imageName, withConfiguration: configuration)
-        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
 }
